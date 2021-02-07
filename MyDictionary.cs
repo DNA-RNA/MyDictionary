a328987c5c076f7bf92a42007e4f87e6f3e75f14
DNA-RNA <77885953+DNA-RNA@users.noreply.github.com>
2021-02-07 21:34:59 +0300

    MyDictionary

---
+++ MyDictionary.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Collections
+{
+    class MyDictionary<T_Key,T_Value>
+    {
+        T_Key[] keys;
+        T_Value[] values;
+
+        public MyDictionary()
+        {
+            keys = new T_Key[0];
+            values = new T_Value[0];
+
+        }
+        public void Add(T_Key key, T_Value value)
+        {
+            T_Key[] temp_Key = keys;
+            T_Value[] temp_Values = values;
+            keys = new T_Key[keys.Length + 1];
+            values = new T_Value[values.Length + 1];
+            for (int i = 0; i < temp_Key.Length; i++)
+            {
+                keys[i] = temp_Key[i];
+                values[i] = temp_Values[i];
+            }
+
+            keys[temp_Key.Length] = key;
+            values[temp_Values.Length] = value;
+
+        }
+        public int Length
+        {
+            get { return keys.Length; }
+        }
+    
+      public void Listing()
+        {
+            for (int i = 0; i < keys.Length; i++)
+            {
+                Console.WriteLine("{0}--->{1}",keys[i],values[i]);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("<<<<<<<<<<-----List Ornekleri----->>>>>>>>>>");
+            
+            List<string> sayilar = new List<string>();  //Hazır kullanılan liste örnek
+            sayilar.Add("101");
+
+            MyList<string> isimler = new MyList<string>(); //Mylist isimiyle oluşturulan generic class listesine örnek
+            isimler.Add("Rana");
+            isimler.Add("Yalçın");
+
+            Console.WriteLine(isimler.Length); //Eklediğimiz eleman sayısını yazdırır.
+
+            foreach (var isim in isimler.Items)
+            {
+                Console.WriteLine(isim);            //Eklediğimiz elemanları yazdırır.
+            }
+          
+            Console.WriteLine("<<<<<<<<<<-----Dictionary Ornekleri----->>>>>>>>>>");
+           
+            Dictionary<string, int> listem = new Dictionary<string, int>();
+            //Hazır kullanılan Dictionarye isim ve yaşı temsil eden değerler girdim.
+            listem.Add("RNA", 19);
+            listem.Add("Zehra", 24);
+            listem.Add("Ali", 25);
+            foreach (int item in listem.Values)
+            {
+                Console.WriteLine(item);     //Burada sadece yaş kısmını yazdırır.
+            }
+
+            foreach (KeyValuePair<string, int> item in listem)
+            {
+                Console.WriteLine(item.Key +"-->" +item.Value.ToString());  //Burada ise hem isimleri hem de yaşi yazdırır.
+            }
+            Console.WriteLine();
+            Console.WriteLine(">>>>>>>-----MyDictionary-----<<<<<<<");
+            
+            MyDictionary<string, string> product = new MyDictionary<string, string>();
+           //Key kısmına ürünlerin adını value kısmına markalarını tanımladım.
+            product.Add("Bilgisayar", "Apple MacBook");
+            product.Add("Telefon", "Samsung");
+            product.Add("Monitör", "Hp");
+            product.Add("Mouse/Fare", "Microsoft");
+            //Oluşturduğumuz dictionarynin toplam eleman sayısı class kısmında int length olarak tanımlayıp burada çağırdım.
+            Console.WriteLine("Eklenen toplam ürün sayısı: " + product.Length); 
+            Console.WriteLine();
+            product.Listing(); //Girdiğimiz ürünleri ve markalarını listelemek için kullanıdığım metot
+            
+        }
+
+
+       
+
+    }
+}
